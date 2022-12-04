50dbe6beca4cae90f52410c28d03edf87da2700d
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-12-04 23:09:16 +0530

    view

---
+++ src/app/view-news/view-news.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-news',
+  templateUrl: './view-news.component.html',
+  styleUrls: ['./view-news.component.css']
+})
+export class ViewNewsComponent {
+
+  view:any=[]
+}
